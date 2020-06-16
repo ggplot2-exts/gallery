bde798faa70b0bc4053ea16efbee2c3f9001ad4a
Matthew Kay <matthew.kay@gmail.com>
2020-06-15 21:28:06 -0400

    generate thumbnails at retina resolution, closes #101

--- scripts/resize-thumbnails.R
+++ scripts/resize-thumbnails.R
@@ -11,7 +11,7 @@ resize_thumbnail <- function(thumbnail) {
   message("resizing ", thumbnail, "...", appendLF = FALSE)
   x <- image_read(thumbnail)
   info <- image_info(x)[1, ]
-  if (info$width <= 350 && info$height <= 300) {
+  if (info$width <= 700 && info$height <= 600) {
     message("skip")
     return(FALSE)
   }
@@ -20,7 +20,7 @@ resize_thumbnail <- function(thumbnail) {
   file.copy(thumbnail, file.path(ORIGINAL_DIR, basename(thumbnail)), overwrite = TRUE)
   
   # resize and save the image
-  x <- image_resize(x, "350x300")
+  x <- image_resize(x, "700x600")
   image_write(x, thumbnail)
 
   message("done")
